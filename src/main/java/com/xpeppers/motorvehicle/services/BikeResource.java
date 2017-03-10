c31a45b6a5fdb467683c788dc3d907f07a35d9b1
Paolo Latella <paolo.latella@xpeppers.com>
2017-03-10 20:37:33 +0100

    Set the correct response in accord with RFC

--- src/main/java/com/xpeppers/motorvehicle/services/BikeResource.java
+++ src/main/java/com/xpeppers/motorvehicle/services/BikeResource.java
@@ -11,7 +11,6 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xpeppers.motorvehicle.BikeMapper;
 import java.io.IOException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
@@ -22,6 +21,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * REST Web Service
@@ -39,53 +39,51 @@ public class BikeResource {
     public BikeResource() {
     }
     
-    public BikeResource(AmazonDynamoDBClient client) 
-    {
+    public BikeResource(AmazonDynamoDBClient client) {
         this.client = client;
     }
 
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public BikeMapper getBikes(@PathParam("id") String id) 
+    public Response getBikes(@PathParam("id") String id) 
     {
-        System.out.println("GET /bikes/{id}");
+        System.out.println("GET /bikes/"+id);
+        //Mapping resource on DynamoDB table
         DynamoDBMapper db_mapper = new DynamoDBMapper(client);
         BikeMapper bike = new BikeMapper();
         try {
             bike = db_mapper.load(BikeMapper.class, id);
+            //The GET method means retrieve whatever information is identified by the Request-URI.
+            return Response.status(200).entity(bike).build();
         }
         catch (AmazonServiceException ase) 
         {
             System.out.println(ase.getErrorMessage());
+            return Response.status(500).entity("{\"Status\":\"Error\"}").build();
         }
-        return bike;
     }
     
     @PUT
     @Path("{id}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public void putBikes(@PathParam("id") String id, String content) 
+    public Response putBikes(@PathParam("id") String id, String content) throws IOException 
     {
-        try {
-            System.out.println("PUT /bikes/{id}");
+            System.out.println("PUT /bikes/"+id);
+            //Mapping resource on DynamoDB table
             DynamoDBMapper db_mapper = new DynamoDBMapper(client);
             ObjectMapper obj_mapper = new ObjectMapper();
             BikeMapper bike = obj_mapper.readValue(content, BikeMapper.class);
             bike.setId(id);
-            System.out.println(content);
             try {
                 db_mapper.save(bike);
+                //If a new resource is created
+                //the origin server MUST inform the user agent via the 201 response
+                return Response.created(context.getAbsolutePath()).build();
             }
-            catch (AmazonServiceException ase)
-            {
-                System.out.println(ase.getErrorMessage());
-            }   
-        }
-        catch (IOException ex) 
-        {
-            Logger.getLogger(BikeResource.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
+            catch (AmazonServiceException ase) {
+                Logger.getLogger(ase.getErrorMessage());
+                return Response.status(500).build();
+            }  
     }
 }
